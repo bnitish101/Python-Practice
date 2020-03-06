4dc609c2ab8ffd26af248743231876285e49146e
bnitish101 <35606236+bnitish101@users.noreply.github.com>
2020-03-06 22:15:33 +0530

    print special variable __name__

---
+++ Special Variable __name__/calc.py
@@ -0,0 +1,3 @@
+import demo
+print('Calc Says: ', __name__)
+print('Its time to calculate!')
---
+++ Special Variable __name__/demo.py
@@ -0,0 +1,8 @@
+def main():
+    print('Hello world')
+    print('Welcome to Demo')
+    print('Demo Says: ', __name__)
+
+
+if __name__ == '__main__':
+    main()
