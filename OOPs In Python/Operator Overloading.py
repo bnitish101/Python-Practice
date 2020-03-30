8a0014ed19e82314412c1aece2669eab0b5bad81
bnitish101 <35606236+bnitish101@users.noreply.github.com>
2020-03-31 02:34:38 +0530

    overloading __str__() inbuilt magic method

--- OOPs In Python/Operator Overloading.py
+++ OOPs In Python/Operator Overloading.py
@@ -7,22 +7,40 @@ class Student:
         m1 = self.m1 + other.m1
         m2 = self.m2 + other.m2
         s3 = Student(m1, m2)
-
         return s3
+
+    def __gt__(self, other):
+        r1 = self.m1 + self.m2
+        r2 = other.m1 + other.m2
+        if r1 > r2:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return '{} {}'.format(self.m1, self.m2)
 
 
 s1 = Student(2, 4)
-s2 = Student(3, 5)
+s2 = Student(3, 4)
 
 s3 = s1 + s2
 print(s3.m1)
 print(Student.__add__(s1, s2).m2)
 
+if s1 > s2:
+    print('S1 wins\n')
+else:
+    print('S2 wins\n')
+
 print(type(s1))
+print(s1)  # if print anything it'll automatically call __str__() magic method,
+# class Student returns value of m1 and m2 in __str__() method which is overloading by inbuilt __str__() method
 
 x = 2
 y = '2'
 
-int
-print(type(x))
+print('\n', type(x))
 print(type(y))
+print(int.__str__(x))
+print(str.__str__(y))
