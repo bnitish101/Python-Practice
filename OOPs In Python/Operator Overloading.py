59cd617ef6103d381e5953bfd4178ed75fdaed2c
bnitish101 <35606236+bnitish101@users.noreply.github.com>
2020-03-31 02:08:22 +0530

    check the class of objects

--- OOPs In Python/Operator Overloading.py
+++ OOPs In Python/Operator Overloading.py
@@ -1,6 +1,28 @@
-a = '5'
-b = '4'
-c = a + b
-print(type(c))
+class Student:
+    def __init__(self, m1, m2):
+        self.m1 = m1
+        self.m2 = m2
 
-print(str.__add__(a, b))
+    def __add__(self, other):
+        m1 = self.m1 + other.m1
+        m2 = self.m2 + other.m2
+        s3 = Student(m1, m2)
+
+        return s3
+
+
+s1 = Student(2, 4)
+s2 = Student(3, 5)
+
+s3 = s1 + s2
+print(s3.m1)
+print(Student.__add__(s1, s2).m2)
+
+print(type(s1))
+
+x = 2
+y = '2'
+
+int
+print(type(x))
+print(type(y))
